a3093c99c914b4b6aa326e9db4c6d789fc206c17
Armin Maghsoudlou <53925421+ArminMaghsoudlou@users.noreply.github.com>
2021-10-12 13:14:18 -0400

    Create robot.java
    basic functionality to read a csv

---
+++ robot.java
@@ -0,0 +1,15 @@
+import java.io.*;  
+import java.util.Scanner;  
+public class Robot 
+{  
+
+public static void main(String[] args) throws Exception  
+    {  
+        Scanner s = new Scanner(new File("C:\\floorplan.csv"));  
+        s.useDelimiter(",");  
+        while (sc.hasNext()){  
+            System.out.print(sc.next());  
+        }   
+        sc.close();
+    }  
+}  
