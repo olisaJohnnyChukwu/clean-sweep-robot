d317d192b21180e8afde928e372f36108b87c2bb
S-CHATHA <76964982+S-CHATHA@users.noreply.github.com>
2021-10-29 22:29:23 -0500

    Add files via upload

---
+++ CleanSweep/src/main/java/battery/BatteryPercentage.java
@@ -0,0 +1,14 @@
+public class BatteryPercentage {
+	private double CurrentBatteryPercentage;
+	private double LowBatteryPercentage = 15;
+	
+	public double GetBatteryPercentage(){
+		return CurrentBatteryPercentage;
+	}
+
+	public void LowBattery(){
+		if(CurrentBatteryPercentage <= LowBatteryPercentage) {
+			System.out.println("Low Battery Warning: " + CurrentBatteryPercentage);
+		}
+	}
+}
---
+++ CleanSweep/src/main/java/logging/FloorPlan.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FloorPlan {
+	private List<Floor> floors;
+
+	public FloorPlan() {
+		floors = new ArrayList<>();
+	}
+
+	public Floor getFloor(int floorNum) {
+		return floors.get(floorNum);
+	}
+
+	public List<Floor> getFloors() {
+		return this.floors;
+	}
+
+	public void addFloor(Floor f) {
+		this.floors.add(f);
+	}
+
+	public int numberOfFloors() {
+		return this.floors.size();
+	}
+}
